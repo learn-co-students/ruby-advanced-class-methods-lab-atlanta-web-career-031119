a31f88c0e8d48149c681339d9d8a3a7db76c62a4
github username <github email address>
2019-03-15 14:46:06 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
+require 'pry'
 class Song
-  attr_accessor :name, :artist_name
+  attr_reader :name, :artist_name
   @@all = []
+  def initialize(title, artist)
+    @name=title
+    @artist_name=artist
+  end
   def self.all
     return @@all
   end
@@ -28,7 +33,10 @@ class Song
     if self.find_by_name(song)
       return self.find_by_name(song)
     else
-      self.create_by_name(name)
+      self.create_by_name(song)
     end
+  end
+  def self.alphabetical
+    return @@all.name.sort
   end
 end
